fd633f535eb564fbb7e39c62a0870888a55d2593
Murat Karimov <mikarkarimk@hotmail.com>
2024-11-10 18:39:12 +0300

    все изменил

---
+++ Converters/tutdesk.Converters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Avalonia.Data.Converters;
+using System.Globalization;
+
+namespace tutdesk.Converters
+{
+    public class PercentageToWidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int progress)
+            {
+                return 3.0 * progress; // Увеличьте множитель по необходимости для подходящей ширины
+            }
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Models/Course.cs
+++ Models/Course.cs
@@ -12,8 +12,12 @@ namespace tutdesk.Models
         public string? Description { get; set; }   // Описание курса
         public string? Icon { get; set; }          // Путь к изображению иконки курса
 
+        public string? LabelText { get; set; }     // Текст для метки, например "CS50x 2024 SQL"
+        public int Progress { get; set; }          // Прогресс курса в процентах
+
         // Вы можете добавить дополнительные свойства по необходимости, например:
-        public string? Duration { get; set; }      // Длительность курса
-        public string? Level { get; set; }         // Уровень сложности (начальный, средний, продвинутый)
+
+        //  public string? Duration { get; set; }      // Длительность курса
+        //   public string? Level { get; set; }         // Уровень сложности (начальный, средний, продвинутый)
     }
 }
