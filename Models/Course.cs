81ff5bc0ad4550c55e8959f833b41f632acf8e02
Alan <king.python@mail.ru>
2024-11-10 15:08:12 +0300

    баг с курсом исправлен, укуси меня асинхронщина

--- Models/Course.cs
+++ Models/Course.cs
@@ -7,13 +7,13 @@ namespace tutdesk.Models
 {
     public class Course
     {
-        public Task<Bitmap?> ?Image { get; set; } 
-        public string ?Title { get; set; }         // Название курса
-        public string ?Description { get; set; }   // Описание курса
-        public string ?Icon { get; set; }          // Путь к изображению иконки курса
+        public Bitmap? Image { get; set; } 
+        public string? Title { get; set; }         // Название курса
+        public string? Description { get; set; }   // Описание курса
+        public string? Icon { get; set; }          // Путь к изображению иконки курса
 
         // Вы можете добавить дополнительные свойства по необходимости, например:
-        public string ?Duration { get; set; }      // Длительность курса
-        public string ?Level { get; set; }         // Уровень сложности (начальный, средний, продвинутый)
+        public string? Duration { get; set; }      // Длительность курса
+        public string? Level { get; set; }         // Уровень сложности (начальный, средний, продвинутый)
     }
 }
